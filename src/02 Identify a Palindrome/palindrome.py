541d4578803c29591bb289585831733c13379846
uday966666 <43602438+uday966666@users.noreply.github.com>
2024-10-20 14:11:37 +0000

    adding new mthod for finding out palindrome.

--- src/02 Identify a Palindrome/palindrome.py
+++ src/02 Identify a Palindrome/palindrome.py
@@ -1,12 +1,23 @@
 import re
 
 def is_palindrome(phrase):
-    forwards = ''.join(re.findall(r'[a-z]+', phrase.lower()))
-    backwards = forwards[::-1]
-    return forwards == backwards
+    cleaned_text = re.sub(r'[^\w]', '', phrase)  
+    cleaned_text = re.sub(r'\s+', '', cleaned_text) 
+    cleaned_text = cleaned_text.lower()
+    start, end = 0 , len(cleaned_text)-1
+    while start < end:
+        if cleaned_text[start] != cleaned_text[end]:
+            return False
+        start+=1
+        end-=1
+    return True
 
 
 # commands used in solution video for reference
 if __name__ == '__main__':
     print(is_palindrome('hello world'))  # false
     print(is_palindrome("Go hang a salami, I'm a lasagna hog."))  # true
+    print(is_palindrome("go."))  # false
+    print(is_palindrome(""))  # true
+    print(is_palindrome("g"))  # true
+    print(is_palindrome("gOG"))  # true
